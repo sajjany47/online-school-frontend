9a2c641559ec143aefb35da81cba899109055c8e
QbAnurag <anurag.patra@qbent.in>
2025-02-05 16:25:59 +0530

    feat: add SuccessToast component for success notifications with customizable options

--- src/app/_component/Toast.tsx
+++ src/app/_component/Toast.tsx
@@ -11,3 +11,15 @@ export const ErrorToast = (msg: any) => {
     theme: "colored",
   });
 };
+
+export const SuccessToast = (msg: any) => {
+  return toast.success(msg, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: false,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "colored",
+  });
+};
