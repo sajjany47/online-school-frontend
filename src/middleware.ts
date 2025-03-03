ec311186ec4ca9727be901f43ca11eaa88d7ef2b
QbAnurag <anurag.patra@qbent.in>
2025-03-03 18:25:18 +0530

    feat: simplify middleware logic for unprotected routes and ensure proper token extraction and verification

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,13 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { jwtVerify } from "jose"; // ✅ Use jose for Edge compatibility
-import { SecretKey } from "./shared/Constant"; 
+import { SecretKey } from "./shared/Constant";
 
 export async function middleware(req: NextRequest) {
-  const UnProtectedRoutes = ["/api/user/login"];
   const { pathname } = req.nextUrl;
-  UnProtectedRoutes.forEach(element => {
-    if (pathname === element) {
-      return NextResponse.next();
-    }
-  });
- 
+  if (pathname === "/api/user/login") {
+    return NextResponse.next();
+  }
 
   const authHeader = req.headers.get("authorization");
   if (!authHeader) {
@@ -22,7 +18,6 @@ export async function middleware(req: NextRequest) {
   }
 
   try {
-   
     const token = authHeader.split(" ")[1]; // Extract token from header
 
     // ✅ Convert secret key to Uint8Array
@@ -30,7 +25,7 @@ export async function middleware(req: NextRequest) {
 
     // ✅ Verify token using `jose`
     const { payload } = await jwtVerify(token, secret);
-req.user=payload
+    req.user = payload;
 
     // Middleware cannot modify `req`, so pass data via headers or cookies if needed
     return NextResponse.next();
